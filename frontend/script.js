67af1ace9ff381466c8152c6fe30260e746632e4
Muhammad Abdul Rafey Farooqi <abdurafeyf@gmail.com>
2023-04-01 05:35:03 +0500

    csv file path

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-d3.csv("https://raw.githubusercontent.com/abdurafeyf/time-series-analysis/main/data/predicted.csv", function(err, rows){
+d3.csv("https://raw.githubusercontent.com/abdurafeyf/time-series-analysis/main/data/actual-and-forecasted.csv", function(err, rows){
 
   function unpack(rows, key) {
   return rows.map(function(row) { return row[key]; });
@@ -8,18 +8,18 @@ d3.csv("https://raw.githubusercontent.com/abdurafeyf/time-series-analysis/main/d
 var trace1 = {
   type: "scatter",
   mode: "lines",
-  name: 'BTC High',
+  name: 'BTC Actual',
   x: unpack(rows, 'ds'),
-  y: unpack(rows, 'trend_upper'),
+  y: unpack(rows, 'actual'),
   line: {color: '#17BECF'}
 }
 
 var trace2 = {
   type: "scatter",
   mode: "lines",
-  name: 'BTC Low',
+  name: 'BTC Forecasted',
   x: unpack(rows, 'ds'),
-  y: unpack(rows, 'trend_lower'),
+  y: unpack(rows, 'trend'),
   line: {color: '#7F7F7F'}
 }
 
@@ -58,54 +58,3 @@ var layout = {
 Plotly.newPlot('range-slider', data, layout);
 })
 
-
-d3.csv("https://raw.githubusercontent.com/abdurafeyf/time-series-analysis/main/data/actual.csv", function(err, rows){
-
-  function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]; });
-}
-
-
-var trace1 = {
-  type: "scatter",
-  mode: "lines",
-  name: 'BTC Actual',
-  x: unpack(rows, 'ds'),
-  y: unpack(rows, 'y'),
-  line: {color: '#17BECF'}
-}
-
-var data = [trace1];
-
-var layout = {
-  title: 'Time Series with Rangeslider',
-  xaxis: {
-    autorange: true,
-    range: ['2018-04-02', '2024-03-29'],
-    rangeselector: {buttons: [
-        {
-          count: 1,
-          label: '1m',
-          step: 'month',
-          stepmode: 'backward'
-        },
-        {
-          count: 6,
-          label: '6m',
-          step: 'month',
-          stepmode: 'backward'
-        },
-        {step: 'all'}
-      ]},
-    rangeslider: {range: ['2018-04-02', '2024-03-29']},
-    type: 'date'
-  },
-  yaxis: {
-    autorange: true,
-    range: [86.8700008333, 138.870004167],
-    type: 'linear'
-  }
-};
-
-Plotly.newPlot('range-slider-2', data, layout);
-})
